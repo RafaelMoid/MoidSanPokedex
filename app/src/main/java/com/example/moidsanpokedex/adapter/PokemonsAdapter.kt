4282232be60ecb6c77efea346a53d4d3d2d10d5c
Rafael Varela <rafaelvrcr@gmail.com>
2021-08-17 11:16:51 -0300

    Adapter creation and rating atribute added to pokemon.kt

---
+++ app/src/main/java/com/example/moidsanpokedex/adapter/PokemonsAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.moidsanpokedex.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PokemonsAdapter : RecyclerView.Adapter<> {
+
+    class PokemonViewHolder(itemView: View){
+
+        }
+    }
--- app/src/main/java/com/example/moidsanpokedex/model/Pokemon.kt
+++ app/src/main/java/com/example/moidsanpokedex/model/Pokemon.kt
@@ -3,5 +3,6 @@ package com.example.moidsanpokedex.model
 data class Pokemon(
     var codigo: Int,
     var nomeDoPokemon: String,
+    var rating: Int,
     var tipoDoPokemon: String
 )
